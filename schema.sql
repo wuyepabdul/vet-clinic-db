919ebd355d1a05bcad2dfa022d7537f7d15069d7
wuyepabdul <wuyepabdul@gmail.com>
2021-11-29 23:17:26 +0100

    Insert data into vetClinic database

--- schema.sql
+++ schema.sql
@@ -1,3 +1,3 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (id INT GENERATED ALWAYS AS IDENTITY, animal_name VARCHAR(100), date_of_birth DATE, escape_attempts INT, neutered BIT, weight_kg DECIMAL)
+CREATE TABLE animals (id INT GENERATED ALWAYS AS IDENTITY, name VARCHAR(100), date_of_birth DATE, escape_attempts INT, neutered BIT, weight_kg DECIMAL)
