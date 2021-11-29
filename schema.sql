3b10c250d58dfa632d8a8b403cf793e117c515ce
wuyepabdul <wuyepabdul@gmail.com>
2021-11-29 23:51:27 +0100

    remove comments

--- schema.sql
+++ schema.sql
@@ -1,3 +1,2 @@
-/* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (id INT GENERATED ALWAYS AS IDENTITY, name VARCHAR(100), date_of_birth DATE, escape_attempts INT, neutered BIT, weight_kg DECIMAL)
