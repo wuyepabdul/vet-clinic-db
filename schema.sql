d659601cab6c34eb1ae3e69b1f5d30ce7f14d5b1
wuyepabdul <wuyepabdul@gmail.com>
2021-11-30 16:35:18 +0100

    Add species column to animals table

--- schema.sql
+++ schema.sql
@@ -1,2 +1,2 @@
 
-CREATE TABLE animals (id INT GENERATED ALWAYS AS IDENTITY, name VARCHAR(100), date_of_birth DATE, escape_attempts INT, neutered BIT, weight_kg DECIMAL)
+CREATE TABLE animals (id INT GENERATED ALWAYS AS IDENTITY, name VARCHAR(100), date_of_birth DATE, escape_attempts INT, neutered BIT, weight_kg DECIMAL, species TEXT)
