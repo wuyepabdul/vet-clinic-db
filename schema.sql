0e4364add56e49fac3718fbe505e446b15e487df
wuyepabdul <wuyepabdul@gmail.com>
2021-11-30 20:59:55 +0100

    Add more Insert queries to data.sql

--- schema.sql
+++ schema.sql
@@ -1,2 +1,3 @@
+DROP TABLE IF EXISTS animals;
 
-CREATE TABLE animals (id INT GENERATED ALWAYS AS IDENTITY, name VARCHAR(100), date_of_birth DATE, escape_attempts INT, neutered BIT, weight_kg DECIMAL, species TEXT)
+CREATE TABLE animals (id INT GENERATED ALWAYS AS IDENTITY, name VARCHAR(100), date_of_birth DATE, escape_attempts INT, neutered BIT, weight_kg DECIMAL, species TEXT);
