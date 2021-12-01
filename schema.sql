5fa1137b50f9d6a4bd22f99259d13788834e5fc0
wuyepabdul <wuyepabdul@gmail.com>
2021-12-01 14:42:47 +0100

    Add queries to Alter animals table

--- schema.sql
+++ schema.sql
@@ -1,7 +1,15 @@
 DROP TABLE IF EXISTS animals;
 
-CREATE TABLE animals (id INT SERIAL NOT NULL PRIMARY KEY, name VARCHAR(100), date_of_birth DATE, escape_attempts INT, neutered BIT, weight_kg DECIMAL, species_id INT references species(id), owner_id INT references owners(id));
+CREATE TABLE animals (id INT GENERATED ALWAYS AS IDENTITY, name VARCHAR(100), date_of_birth DATE, escape_attempts INT, neutered BIT, weight_kg DECIMAL, species TEXT);
+
 
 CREATE TABLE owners (id SERIAL NOT NULL PRIMARY KEY, full_name VARCHAR(100), age INT);
-
 CREATE TABLE species (id SERIAL NOT NULL PRIMARY KEY, name VARCHAR(100));
+
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD COLUMN species_id INT;
+ALTER TABLE animals ADD COLUMN owner_id INT;
+ALTER TABLE animals ADD FOREIGN KEY (species_id) REFERENCES species(id);
+ALTER TABLE animals ADD FOREIGN KEY (owner_id) REFERENCES owners(id);
+
+
