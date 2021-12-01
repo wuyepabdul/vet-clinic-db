91d4e5991a26dedc48ab3d9e5ffbeb2edfa687fa
wuyepabdul <wuyepabdul@gmail.com>
2021-12-01 14:07:00 +0100

    Add queries to create owners and species table and modify animals table in schema.sql

--- schema.sql
+++ schema.sql
@@ -1,3 +1,7 @@
 DROP TABLE IF EXISTS animals;
 
-CREATE TABLE animals (id INT GENERATED ALWAYS AS IDENTITY, name VARCHAR(100), date_of_birth DATE, escape_attempts INT, neutered BIT, weight_kg DECIMAL, species TEXT);
+CREATE TABLE animals (id INT SERIAL NOT NULL PRIMARY KEY, name VARCHAR(100), date_of_birth DATE, escape_attempts INT, neutered BIT, weight_kg DECIMAL, species_id INT references species(id), owner_id INT references owners(id));
+
+CREATE TABLE owners (id SERIAL NOT NULL PRIMARY KEY, full_name VARCHAR(100), age INT);
+
+CREATE TABLE species (id SERIAL NOT NULL PRIMARY KEY, name VARCHAR(100));
