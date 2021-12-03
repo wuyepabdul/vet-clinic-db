ed21d762a862947812060be7bcee484a6a0078a2
wuyepabdul <wuyepabdul@gmail.com>
2021-12-03 01:15:44 +0100

    Add queries to schema.sql

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS animals;
 
-CREATE TABLE animals (id INT GENERATED ALWAYS AS IDENTITY, name VARCHAR(100), date_of_birth DATE, escape_attempts INT, neutered BIT, weight_kg DECIMAL, species TEXT);
+CREATE TABLE animals (id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, name VARCHAR(100), date_of_birth DATE, escape_attempts INT, neutered BIT, weight_kg DECIMAL, species TEXT);
 
 --Create a table named owners with the following columns:
 CREATE TABLE owners (id SERIAL NOT NULL PRIMARY KEY, full_name VARCHAR(100), age INT);
@@ -21,4 +21,23 @@ ALTER TABLE animals ADD COLUMN owner_id INT;
 ALTER TABLE animals ADD FOREIGN KEY (species_id) REFERENCES species(id);
 ALTER TABLE animals ADD FOREIGN KEY (owner_id) REFERENCES owners(id);
 
+/* 
+Create a table named vets with the following columns:
+id: integer (set it as autoincremented PRIMARY KEY)
+name: string
+age: integer
+date_of_graduation: date
+*/
+CREATE TABLE vets (id SERIAL NOT NULL PRIMARY KEY, name VARCHAR(100), age INT, date_of_graduation DATE);
 
+/* 
+Create a "join table" called specializations to handle many-to-many relationship
+between vets and species
+*/
+CREATE TABLE specializations (id SERIAL NOT NULL PRIMARY KEY, species_id INT, vet_id INT, FOREIGN KEY(species_id) REFERENCES species(id),  FOREIGN KEY(vet_id) REFERENCES vets(id));
+
+/* 
+Create a "join table" called visits to handle many-to-many relationship
+between vets and animals
+*/
+CREATE TABLE visits (id SERIAL NOT NULL PRIMARY KEY, visit_date DATE, vet_id INT, animal_id INT, CONSTRAINT fk_vet FOREIGN KEY(vet_id) REFERENCES vets(id), CONSTRAINT fk_animal FOREIGN KEY(animal_id) REFERENCES animals(id));
